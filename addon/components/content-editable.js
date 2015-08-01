bd13616ec7a93e46fb61df29947f90d693a17d62
Chris Sloey <chris@addjam.com>
2015-08-01 11:40:29 +0100

    Add support for bubbling events including escape-press and enter #1

--- addon/components/content-editable.js
+++ addon/components/content-editable.js
@@ -12,7 +12,41 @@ export default Ember.Component.extend({
 
   stringInterpolator(s) { return s; },
 
-  updateValue: Ember.on('keyUp', function() {
+  updateValue: Ember.on('keyUp', function(event) {
+    this.handleKeyUp(event);
     this.set('value', this.stringInterpolator(this.$().text()));
-  })
+  }),
+
+  handleKeyUp(event) {
+    this.sendAction('keyUp', event);
+
+    if (event.keyCode === 27) {
+      // Escape
+      this.sendAction('escape-press', event);
+    } else if (event.keyCode === 13) {
+      // Enter
+      this.sendAction('enter', event);
+    }
+  },
+
+  /* Events */
+  keyDown(event) {
+    this.sendAction('key-down', event);
+  },
+
+  focusIn(event) {
+    this.sendAction('focus-in', event);
+  },
+
+  focusOut(event) {
+    this.sendAction('focus-out', event);
+  },
+
+  mouseEnter(event) {
+    this.sendAction('mouse-enter', event);
+  },
+
+  mouseLeave(event) {
+    this.sendAction('mouse-leave', event);
+  },
 });
