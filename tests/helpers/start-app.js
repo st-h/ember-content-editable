1f06cad40fa8864b92789868784bebc698824284
Gaurav Munjal <gmunjal@linkedin.com>
2016-10-07 07:49:25 -0700

    Update ember cli to 2.8.0

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -4,12 +4,12 @@ import config from '../../config/environment';
 import registerHelpers from './register-helpers';
 
 export default function startApp(attrs) {
-  var application;
+  let application;
 
-  var attributes = Ember.merge({}, config.APP);
+  let attributes = Ember.merge({}, config.APP);
   attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
 
-  Ember.run(function() {
+  Ember.run(() => {
     application = Application.create(attributes);
     registerHelpers();
     application.setupForTesting();
