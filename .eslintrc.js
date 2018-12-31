31a10812d23ef13c1923f5918bf4a3281b446ea9
steve <steve.hummingbird@yandex.com>
2018-12-31 17:35:44 +0100

    update dependencies version 1.0.3

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,9 @@ module.exports = {
     // node files
     {
       files: [
+        '.eslintrc.js',
+        '.template-lintrc.js',
+        'ember-cli-build.js',
         'index.js',
         'testem.js',
         'ember-cli-build.js',
